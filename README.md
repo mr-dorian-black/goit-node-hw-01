4fcc1c7a054dcc146f1bb6420ad08081e8dabb33
Dorian Black <d.black.personal@gmail.com>
2023-12-11 15:15:54 +0200

    v1.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+[node index.js --action list]
+(https://monosnap.com/file/MbZwdgfIAdzzh6fzzn7vtKwT80zoxl)
+
+# Получаем контакт по id - выводим в консоль объект контакта или null, если контакта с таким id не существует.
+
+[node index.js --action get --id 05olLMgyVQdWRwgKfg5J6]
+(https://monosnap.com/file/TA7yHxU26V2CO0RmBP1eOAYmTXvcAc)
+
+# Добавляем контакт и выводим в консоль созданный контакт
+
+[node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22]
+(https://monosnap.com/file/W14VzuT3QU17BrNADJ6uXoNEbyrrMM)
+
+# Удаляем контакт и выводим в консоль удаленный контакт или null, если контакта с таким id не существует.
+
+[node index.js --action remove --id qdggE76Jtbfd9eWJHrssH]
+(https://monosnap.com/file/NhfEjJ7NFQnKLRJnlChYfHn3l5W4hk)
---
+++ index.js
@@ -0,0 +1,33 @@
+import * as contactsService from "./db/index.js";
+import { program } from "commander";
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      return console.log(allContacts);
+    case "get":
+      const contact = await contactsService.getContactById(id);
+      return console.log(contact);
+    case "add":
+      const newContact = await contactsService.addContact(name, email, phone);
+      return console.log(newContact);
+    case "remove":
+      const removeContact = await contactsService.removeContact(id);
+      return console.log(removeContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-e, --email <type>")
+  .option("-p, --phone <type>");
+
+program.parse();
+
+const options = program.opts();
+invokeAction(options);
